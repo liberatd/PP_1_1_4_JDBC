b46cc82a20465d4d7bd38e71d09c70049885ef97
Oleg Vologin <146234675+liberatd@users.noreply.github.com>
2023-11-28 20:48:29 +0400

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/Test1";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "Vol22918";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection () {
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
